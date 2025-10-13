18cc716ca106533838f61eaf32e5dc6e3a28cad2
d1rshan <darshan.paccha@gmail.com>
2025-10-13 14:53:31 +0530

    feat: configure trpc for protected procedures

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { db } from "@/db";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => {
-    return db.query.usersTable.findMany();
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    return db.query.user.findFirst({
+      where: (user, { eq }) => eq(user.id, ctx.auth.user.id),
+    });
   }),
 });
 // export type definition of API
