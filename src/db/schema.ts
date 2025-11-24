eadcd7d29d3cc9f296dbc7b18156773641cb10bc
d1rshan <darshan.paccha@gmail.com>
2025-11-24 21:21:27 +0530

    feat: finish node selector

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -81,6 +81,8 @@ export const workflow = pgTable("workflow", {
 
 export const NodeType = {
   INITIAL: "INITIAL",
+  MANUAL_TRIGGER: "MANUAL_TRIGGER",
+  HTTP_REQUEST: "HTTP_REQUEST"
 } as const;
 
 export type NodeType = (typeof NodeType)[keyof typeof NodeType];
